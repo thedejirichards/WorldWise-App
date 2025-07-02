75fcc4f1c561d40266b444babc11ca7735a017d9
Richard  Oladeji <oladejir@accessbankplc.com>
2025-07-02 05:49:14 +0100

    After changing theit jsx files to tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import styles from "./Login.module.css";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState<string>("jack@example.com");
+  const [password, setPassword] = useState<string>("qwerty");
 
   return (
     <main className={styles.login}>
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav"
-
-function PageNotFound() {
-    return (
-        <div>
-            <PageNav/>
-            <div>Page Not Found</div>
-        </div>
-    )
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound
